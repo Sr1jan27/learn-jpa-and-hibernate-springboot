effa72f8039f123bed077b841543fb6df16205ea
srijan_singh <singhsrijan809@gmail.com>
2024-06-25 10:05:35 +0530

    changes for jdbc connection

---
+++ src/main/java/com/srijan/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,18 @@
+package com.srijan.springboot.learn_jpa_and_hibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class CourseJdbcCommandLineRunner implements CommandLineRunner{
+	
+	@Autowired
+	private CourseJdbcRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repository.insert();
+		
+	}
+
+}
--- src/main/java/com/srijan/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/srijan/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,17 +1,19 @@
 package com.srijan.springboot.learn_jpa_and_hibernate.course.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CourseJdbcRepository {
-
+		
+		@Autowired
 		private JdbcTemplate springJdbcTemplate;
 		
 		private static String INSERT_QUERY  = 
 				"""
 					insert into course (id, name, author)
-					values(1, 'Learn AWS', 'Srijan');
+					values(2, 'Learn AWS2', 'Srijan2');
 				""";
 		
 		public void insert() {
