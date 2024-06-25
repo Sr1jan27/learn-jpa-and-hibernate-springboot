f3a95a5927b380ae5e785bf54dfeb6a96170bae5
srijan_singh <singhsrijan809@gmail.com>
2024-06-25 09:56:26 +0530

    doing jdbc things

---
+++ src/main/java/com/srijan/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,20 @@
+package com.srijan.springboot.learn_jpa_and_hibernate.course.jdbc;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+
+		private JdbcTemplate springJdbcTemplate;
+		
+		private static String INSERT_QUERY  = 
+				"""
+					insert into course (id, name, author)
+					values(1, 'Learn AWS', 'Srijan');
+				""";
+		
+		public void insert() {
+			springJdbcTemplate.update(INSERT_QUERY);
+		}
+}
